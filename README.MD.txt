239c2e9125b118dd022e0d05259b7baab5a87196
Mohit Patni <mohitpatni0731@gmail.com>
2021-03-10 22:25:50 +0530

    Add files via upload

---
+++ README.MD.txt
@@ -0,0 +1,2 @@
+Hey
+I've created this Random Password Generator which basically ask from user to enter the length of the password and how many characters user want in the password , then it generates random password.
---
+++ randompasswordgenerator.py
@@ -0,0 +1,23 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
+
+#while 1:
+try:
+    password_len = int(input("What length would you like your password to be -"))
+except:
+    print("Wrong Input, Type Integer only")
+    pass
+try:
+    password_count = int(input("How many password would you want -"))
+except:
+    print("Wrong Input, Type Integer only")
+    pass
+for x in range(0,password_count):
+    password = ""
+    for x in range(0,password_len):
+        password_char = random.choice(chars)
+        password = password + password_char
+    print("Here you go -", password)
+if password_len == str:
+    print("Wrong")
